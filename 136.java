34f7c8701af35dfc1f78d56453cb2cf31b4712e3
Ivanz7 <yifanzhu@umass.edu>
2017-03-24 23:54:20 -0400

    3/18

--- 136.java
+++ 136.java
@@ -7,7 +7,7 @@ public class Solution {
       else for(int i=0;i<nums.length-1;i=i+2){
           if(nums[i]!=nums[i+1]){
               return nums[i];
-      }
+          }
       }
       return nums[nums.length-1];
     }
