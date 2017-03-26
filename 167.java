33274b0cf159f5556cb776443602b01906d905f3
Ivanz7 <yifanzhu@umass.edu>
2017-03-26 14:18:03 -0400

    3/26

---
+++ 167.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int a = 0;
+        int b = numbers.length-1;
+        while(a<b){
+            if((numbers[a]+numbers[b])==target){
+                break;
+            }
+            if((numbers[a]+numbers[b])<target){
+                a++;
+            }
+            else{
+                b--;
+            }
+        }
+        return new int[]{a+1,b+1};
+    }
+}
