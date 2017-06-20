0e2208efc6ee8f65cd733a8743f22494bb8e5a25
Ivanz7 <yifanzhu@umass.edu>
2017-06-20 16:11:04 -0400

    6/20

---
+++ 598.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int maxCount(int m, int n, int[][] ops) {
+       if(ops==null||ops.length==0){
+           return m*n;
+       }
+       int row = m, col = n;
+       for(int[] op: ops){
+           row = Math.min(row, op[0]);
+           col = Math.min(col, op[1]);
+       }
+       
+       return row*col; 
+    }
+}
