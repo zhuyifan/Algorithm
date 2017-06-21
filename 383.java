28d6c8faa608ab7f94842648a290b70ab35ba133
Ivanz7 <yifanzhu@umass.edu>
2017-06-21 17:17:23 -0400

    6/21

---
+++ 383.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+       int[] arr = new int[26];
+       for(int i=0; i< magazine.length();i++){
+           arr[magazine.charAt(i)-'a']++;
+       }
+       for(int i=0;i< ransomNote.length();i++){
+           arr[ransomNote.charAt(i)-'a']--;
+       }
+       for(int i=0;i<26;i++){
+           if(arr[i]<0){
+               return false;
+           }
+       }
+        return true;
+    }
+}
