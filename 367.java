f256311800416a5adcf11c03141cb50704fc884b
Ivanz7 <yifanzhu@umass.edu>
2017-06-26 03:41:18 -0400

    6、26

--- 367.java
+++ 367.java
@@ -7,4 +7,27 @@ public class Solution {
       }
       return num==0;
     }
+
+     public boolean isPerfectSquare1(int num) {
+      int low = 1, high = num;
+        while (low <= high) {
+            long mid = (low + high)/2;
+            if (mid * mid == num) {
+                return true;
+            } else if (mid * mid < num) {
+                low = (int) mid + 1;
+            } else {
+                high = (int) mid - 1;
+            }
+        }
+        return false;
+    }//binary search
+
+    public boolean isPerfectSquare2(int num) {
+        long x = num;
+        while (x * x > num) {
+            x = (x + num / x)/2;
+        }
+        return x * x == num;
+    }//Newton Method
 }
