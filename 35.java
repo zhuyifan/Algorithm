44761600e588afc343eb7df061ce8aadab04be2f
Ivanz7 <yifanzhu@umass.edu>
2017-06-25 22:10:51 -0400

    6/25

---
+++ 35.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>=target){
+                return i;
+            }
+        }
+        return nums.length;
+    }
+
+
+public int searchInsert1(int[] A, int target) {
+        int low = 0, high = A.length-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(A[mid] == target) return mid;
+            else if(A[mid] > target) high = mid-1;
+            else low = mid+1;
+        }
+        return low;
+    }//binary search
+
+}
