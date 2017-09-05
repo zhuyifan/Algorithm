2b85591e9eba436a96244441e512eddf38ca65b7
Ivanz7 <yifanzhu@umass.edu>
2017-09-05 16:53:21 -0400

    9/5

---
+++ 447.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numberOfBoomerangs(int[][] points) {
+        int res=0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<points.length; i++) {
+            for(int j=0; j<points.length; j++) {
+                if(i == j)
+                    continue;
+                int d = getDistance(points[i], points[j]);                
+                map.put(d, map.getOrDefault(d, 0) + 1);
+            }
+        
+        for(int val : map.values()) {
+            res += val * (val-1);
+        }            
+        map.clear();
+    }
+    
+    return res;
+        
+    }
+    public int getDistance(int[] a, int[] b){
+        return (a[0]-b[0])*(a[0]-b[0])+(a[1]-b[1])*(a[1]-b[1]);
+    }
+}
